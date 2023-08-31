4917017e2aefeb69162ea43f3a4f8e4644ae40fe
Laurent Teyssier <lau.tey@outlook.fr>
2023-08-31 13:40:05 +0200

    misc

--- resultat.php
+++ resultat.php
@@ -1,27 +1,32 @@
 <?php
 if(isset($_POST['nombre1'])&& isset($_POST['nombre2'])&& !empty($_POST['operator'])){
     if(!is_nan($_POST['nombre1'])&& !is_nan($_POST['nombre2'])){
+        
     $nombre1 = $_POST['nombre1'];
     $nombre2 = $_POST['nombre2'];
     $operator = $_POST['operator'];
-
+    
     // Moins de code
-    $result = eval("return $nombre1 $operator $nombre2;");
+    // $result = eval("return $nombre1 $operator $nombre2;");
 
     //Plus propre
-    // $result = null;
-    // if($operator == "*") {
-    //     $result = $nombre1 * $nombre2;
-    // } elseif($operator == "/") {
-    //     $result = $nombre1 / $nombre2;
-    // } elseif($operator == "+") {
-    //     $result = $nombre1 + $nombre2;
-    // }else($operator == "-") {
-    //     $result = $nombre1 - $nombre2;
+    $result = null;
+    if($operator == "*") {
+        $result = $nombre1 * $nombre2;
+    } elseif($operator == "/") {
+        if ($operator == "/" && $nombre2==0)  {
+            echo "Division par 0 impossible";
+        }
+        $result = $nombre1 / $nombre2;
+    } elseif($operator == "+") {
+        $result = $nombre1 + $nombre2;
+    }else{
+        $result = $nombre1 - $nombre2;
+    }
 
 
     echo "$nombre1 $operator $nombre2 = $result";
-} else {
+} else{
     echo "Veuillez entrer des nombres";
 }
 }
